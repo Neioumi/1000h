a56735491c3b55f1e78599d2ae51a54358174625
Yuki <paselan@gmail.com>
2014-02-22 03:05:57 +0900

    Implement SheetProperty instead of SheetStorage

--- spec/SheetPropertySpec.js
+++ spec/SheetPropertySpec.js
@@ -5,6 +5,7 @@ describe("SheetProperty", function () {
 
   describe(".createId", function () {
     it("returns 1 when id_sequence not exists", function () {
+      SheetProperty.remove(1);
       expect(SheetProperty.createId()).toEqual(1);
     });
 
@@ -19,6 +20,17 @@ describe("SheetProperty", function () {
     });
   });
 
+  describe(".index", function () {
+    it("returns sheet index when index exists", function () {
+      localStorage.setItem("sheet_index", "[1,2]");
+      expect(SheetProperty.index()).toEqual([1, 2]);
+    });
+
+    it("returns an empty array when index does not exist", function () {
+      expect(SheetProperty.index()).toEqual([]);
+    });
+  });
+
   describe("new sheet", function () {
     it("creates new property", function () {
       var property = new SheetProperty(1);
@@ -28,17 +40,20 @@ describe("SheetProperty", function () {
     });
   });
 
-  describe("existent sheet", function () {
-    it("loads properties", function () {
+  describe(".load", function () {
+    var data;
+    beforeEach(function () {
       var date = new Date();
-      var data = {
+      data = {
         title: "title for sheet1",
         startDate: date.getTime(),
         goalDate: date.getTime() + 86400,
       };
       localStorage.setItem("sheet[1]", JSON.stringify(data));
+    });
 
-      var property = new SheetProperty(1);
+    it("loads properties", function () {
+      var property = SheetProperty.load(1);
       expect(property.title).toEqual(data.title);
       expect(property.startDate).toEqual(jasmine.any(Date));
       expect(property.startDate.getTime()).toEqual(data.startDate);
@@ -47,30 +62,62 @@ describe("SheetProperty", function () {
     });
   });
 
-  describe("update property", function () {
-    it("saves properties", function () {
-      var property = new SheetProperty(1);
-      property.title = "title for sheet1";
+  describe("#save", function () {
+    var date, property;
 
+    beforeEach(function () {
+      date = new Date();
+      property = new SheetProperty(1, {
+        title: "title for sheet1",
+        startDate: date,
+        goalDate: new Date(date.getTime() + 86400)
+      });
+    });
+
+    it("saves properties", function () {
       var expected = JSON.stringify({
         title: "title for sheet1",
-        startDate: null,
-        goalDate: null
+        startDate: date.getTime(),
+        goalDate: date.getTime() + 86400
       });
 
-      expect(property.title).toEqual("title for sheet1");
+      property.save();
       expect(localStorage.getItem("sheet[1]")).toEqual(expected);
+    });
+
+    it("adds sheet ID into sheet index when it has not been indexed", function () {
+      property.save();
+      expect(localStorage.getItem("sheet_index")).toEqual("[1]");
+    });
+
+    it("doesn't add sheet ID into sheet index when it has been indexed", function () {
+      localStorage.setItem("sheet_index", "[1]");
+      property.save();
+      expect(localStorage.getItem("sheet_index")).toEqual("[1]");
     });
   });
 
   describe("#remove", function () {
+    var property;
+
+    beforeEach(function () {
+      property = new SheetProperty(1, {
+        title: "title for sheet1"
+      });
+    });
+
     it("removes all data associated with the sheet", function () {
-      var property = new SheetProperty(1);
-      property.title = "title for sheet1";
+      property.save();
       expect(localStorage.getItem("sheet[1]")).not.toBeNull();
 
       property.remove();
       expect(localStorage.getItem("sheet[1]")).toBeNull();
+    });
+
+    it("removes sheet ID from sheet index when it has been indexed", function () {
+      localStorage.setItem("sheet_index", "[1,2]");
+      property.remove();
+      expect(localStorage.getItem("sheet_index")).toEqual("[2]");
     });
   });
 });
--- src/js/SheetProperty.js
+++ src/js/SheetProperty.js
@@ -17,45 +17,51 @@ var _NODE_JS = !!global.process;
 // --- interface -------------------------------------------
 
 /**
- * Create a new property.
+ * Create a new property object.
  *
  * @constructor
  * @param {Number} sheetId sheet ID
+ * @param {Object} prop properties
  */
-function SheetProperty(sheetId) {
+function SheetProperty(sheetId, prop) {
+  if (prop === undefined) {
+    prop = {};
+  }
+
   this.sheetId    = sheetId;
-  this.storageKey = "sheet[" + sheetId.toString() + "]";
+  this.storageKey = makeStorageKey(sheetId);
   this.prop = {
-    title     : null,
-    startDate : null,
-    goalDate  : null
+    title     : prop.title     == null ? null : prop.title,
+    startDate : prop.startDate == null ? null : prop.startDate,
+    goalDate  : prop.goalDate  == null ? null : prop.goalDate
   };
-  this.load();
 }
 
 SheetProperty.createId = SheetProperty_createId;
+SheetProperty.index    = SheetProperty_index;
+SheetProperty.load     = SheetProperty_load;
+SheetProperty.remove   = SheetProperty_remove;
 
 SheetProperty.prototype = {
-       load: SheetProperty_load,
        save: SheetProperty_save,
-     remove: SheetProperty_remove
+     remove: function () { SheetProperty.remove(this.sheetId); }
 };
 
 Object.defineProperties(SheetProperty.prototype, {
   "title": {
     enumerable: true,
     get: function ()      { return this.prop.title; },
-    set: function (value) { this.prop.title = value; this.save(); }
+    set: function (value) { this.prop.title = value; }
   },
   "startDate": {
     enumerable: true,
     get: function ()      { return this.prop.startDate; },
-    set: function (value) { this.prop.startDate = value; this.save(); }
+    set: function (value) { this.prop.startDate = value; }
   },
   "goalDate": {
     enumerable: true,
     get: function ()      { return this.prop.goalDate; },
-    set: function (value) { this.prop.goalDate = value; this.save(); }
+    set: function (value) { this.prop.goalDate = value; }
   }
 });
 
@@ -72,33 +78,79 @@ function SheetProperty_createId() {
   return new_id;
 }
 
-function SheetProperty_load() {
-  var data = Storage.getJSON(this.storageKey, {});
-  this.prop.title = data.title === undefined ? null : data.title;
-  ["startDate", "goalDate"].forEach(function (key) {
-    this.prop[key] = timestamp_to_date(data[key]);
-  }, this);
+function SheetProperty_index() {
+  return Storage.getJSON("sheet_index", []);
 }
 
+/**
+ * Load sheet property associated with sheet ID.
+ *
+ * @param {Number} sheetId sheet ID
+ * @return {SheetProperty} loaded object
+ */
+function SheetProperty_load(sheetId) {
+  var prop = {},
+      data = Storage.getJSON(makeStorageKey(sheetId), {});
+
+  prop.title = data.title === undefined ? null : data.title;
+  ["startDate", "goalDate"].forEach(function (key) {
+    prop[key] = timestampToDate(data[key]);
+  });
+
+  return new SheetProperty(sheetId, prop);
+}
+
+/**
+ * Save property into storage.
+ */
 function SheetProperty_save() {
   var data = {
     title: this.prop.title
   };
   ["startDate", "goalDate"].forEach(function (key) {
-    data[key] = date_to_timestamp(this.prop[key]);
+    data[key] = dateToTimestamp(this.prop[key]);
   }, this);
+
   Storage.setJSON(this.storageKey, data);
+  addToIndex(this.sheetId);
 }
 
-function SheetProperty_remove() {
-  Storage.remove(this.storageKey);
+/**
+ * Remove propety from storage.
+ */
+function SheetProperty_remove(sheetId) {
+  removeFromIndex(sheetId);
+  Storage.remove(makeStorageKey(sheetId));
 }
 
-function date_to_timestamp(date) {
+// --- implement (private) ---------------------------------
+
+function addToIndex(sheetId) {
+  var index = Storage.getJSON("sheet_index", []);
+  if (index.indexOf(sheetId) == -1) {
+    index.push(sheetId);
+    Storage.setJSON("sheet_index", index);
+  }
+}
+
+function removeFromIndex(sheetId) {
+  var index = Storage.getJSON("sheet_index", []),
+      pos   = index.indexOf(sheetId);
+  if (pos != -1) {
+    index.splice(pos, 1);
+    Storage.setJSON("sheet_index", index);
+  }
+}
+
+function makeStorageKey(sheetId) {
+  return "sheet[" + sheetId.toString() + "]";
+}
+
+function dateToTimestamp(date) {
   return date == null ? null : date.getTime();
 }
 
-function timestamp_to_date(timestamp) {
+function timestampToDate(timestamp) {
   return timestamp == null ? null : new Date(Number(timestamp));
 }
 
